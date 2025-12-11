efadef052c7526f48ef8efeb3707023701cc50d9
LordPizle <aa9620i@gre.ac.uk>
2025-12-11 11:59:59 +0000

    Updated location.py so that each restaurant now includes a picture

--- app/location.py
+++ app/location.py
@@ -3,6 +3,20 @@ import requests
 
 GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
 
+
+def get_photo_url(photo_reference, max_width=400):
+    
+    if not photo_reference:
+        return None
+
+    return (
+        "https://maps.googleapis.com/maps/api/place/photo"
+        f"?maxwidth={max_width}"
+        f"&photoreference={photo_reference}"
+        f"&key={GOOGLE_PLACES_API_KEY}"
+    )
+
+
 def search_nearby_restaurants(lat, lng, keyword):
 
     if not GOOGLE_PLACES_API_KEY:
@@ -23,11 +37,19 @@ def search_nearby_restaurants(lat, lng, keyword):
     restaurants = []
 
     for place in data.get("results", []):
+        
+        photo_ref = None
+        if "photos" in place:
+            photo_ref = place["photos"][0].get("photo_reference")
+
+        photo_url = get_photo_url(photo_ref)
+
         restaurants.append({
             "name": place.get("name"),
             "address": place.get("formatted_address"),
             "rating": place.get("rating"),
             "types": place.get("types"),
+            "photo_url": photo_url,
         })
 
     return restaurants
