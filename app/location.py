f023caa0f1f39eb16448d47ef8453ca3378ba6f3
LordPizle <aa9620i@gre.ac.uk>
2025-12-15 20:10:37 +0000

    Updated code to show how far each restaurant is from the user

--- app/location.py
+++ app/location.py
@@ -1,8 +1,29 @@
 import os
 import requests
+import math
 
 GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
 
+def haversine_distance(lat1, lon1, lat2, lon2):
+    
+    R = 6371
+
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
+
+    d_lat = math.radians(lat2 - lat1)
+    d_lon = math.radians(lon2 - lon1)
+
+    a = (math.sin(d_lat / 2) ** 2 +
+         math.cos(math.radians(lat1)) *
+         math.cos(math.radians(lat2)) *
+         math.sin(d_lon / 2) ** 2)
+
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return round(R * c, 1)
 
 def get_photo_url(photo_reference, max_width=400):
 
@@ -46,6 +67,11 @@ def search_nearby_restaurants(lat, lng, keyword):
         opening_hours = place.get("opening_hours", {})
         open_now = opening_hours.get("open_now")
 
+        place_lat = place["geometry"]["location"]["lat"]
+        place_lng = place["geometry"]["location"]["lng"]
+
+        distance_km = haversine_distance(lat, lng, place_lat, place_lng)
+
         restaurants.append({
             "name": place.get("name"),
             "address": place.get("formatted_address"),
@@ -53,6 +79,7 @@ def search_nearby_restaurants(lat, lng, keyword):
             "types": place.get("types"),
             "photo_url": photo_url,
             "open_now": open_now,
+            "distance_km": distance_km   
         })
 
     return restaurants
